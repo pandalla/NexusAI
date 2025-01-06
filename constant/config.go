5cfc761e5a68a615dc8bc81267192d1281314935
loiosunrise <loiosunrisek@gmail.com>
2025-01-06 22:22:47 +0800

    start: Initialize code warehouse and code framework

---
+++ constant/config.go
@@ -0,0 +1,8 @@
+package constant
+
+var Configs = map[string]string{
+	"frontend_port": "11000",
+	"backend_port":  "10000",
+	"log_max_count": "100000000",
+	"log_dir":       "./logs",
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 设置gin模式
+	gin.SetMode(gin.ReleaseMode)
+}
