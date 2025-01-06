c47d9276f5ef47ea670fb609f025735bda9a81d0
loiosunrise <loiosunrisek@gmail.com>
2025-01-06 22:22:47 +0800

    start: Initialize code warehouse and code framework

---
+++ utils/log.go
@@ -0,0 +1,59 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+
+	"nexus-ai/constant"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	logINFO  = "INFO"
+	logWARN  = "WARN"
+	logERROR = "ERROR"
+)
+
+var maxLogCount = constant.Configs["log_max_count"]
+var logDir = constant.Configs["log_dir"]
+
+var logCount int
+var setupLogWorking bool
+var setupLogLock sync.Mutex
+
+func SetupLog() {
+	if logDir != "" {
+		ok := setupLogLock.TryLock()
+		if !ok {
+			log.Println("SetupLog is working")
+			return
+		}
+		defer func() {
+			setupLogLock.Unlock()
+			setupLogWorking = false
+		}()
+		logPath := filepath.Join(logDir, fmt.Sprintf("nexus-ai-%s.log", time.Now().Format("2006-01-02")))
+		fd, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			log.Fatal("SetupLog failed to open log file:", err)
+		}
+		gin.DefaultWriter = io.MultiWriter(os.Stdout, fd)
+		gin.DefaultErrorWriter = io.MultiWriter(os.Stderr, fd)
+	}
+}
+
+func SysLog(s string) {
+	t := time.Now().Format("2006-01-02 15:04:05")
+	_, _ = fmt.Fprintf(gin.DefaultWriter, "[SYSLOG] %v | %s \n", t, s)
+}
+
+func SysError(s string) {
+	t := time.Now().Format("2006-01-02 15:04:05")
+	_, _ = fmt.Fprintf(gin.DefaultErrorWriter, "[SYSERR] %v | %s \n", t, s)
+}
